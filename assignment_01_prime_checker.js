db69e7538bac1fd518324caddd4ddc2995c67285
opoku-design <opokuamod@gmail.com>
2026-08-07 16:04:10 -0700

    assigment1

--- assignment_01_prime_checker.js
+++ assignment_01_prime_checker.js
@@ -43,3 +43,30 @@
 // =============================================================================
 
 
+const readlineSync = require('readline-sync');
+
+function isPrime(n) {
+    if (n < 2) {
+        return false;
+    }
+
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function main() {
+    const number = readlineSync.questionInt("Enter a number: ");
+
+    if (isPrime(number)) {
+        console.log(`${number} is a prime number.`);
+    } else {
+        console.log(`${number} is NOT a prime number.`);
+    }
+}
+
+main();
